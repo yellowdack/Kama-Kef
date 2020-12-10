2598430bd33d47c266fd20c105f46e13d8801bca
yellowdack <bigyellowduck2019@gmail.com>
2020-12-10 16:01:46 +0200

    add logo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import logo from "../images/logo.png";
 
 function App() {
-  return ();
+  return (
+    <div>
+      <img src={logo} alt="Logo" />;
+      <h1 className ="h1">Welcome To The Kaman Portal!
+      </h1>
+    </div>
+
+  );
 }
+
+
 
 export default App;
--- src/functionHomePage.js
+++ src/functionHomePage.js
@@ -1,9 +1,12 @@
 const functionHomePage =[
   {
     id: 1,
-  }
+    img: "./images/script",
+    name: "Scripts"
+  },
   {
-    id: 2, 
+    id: 2,
+    img: "./images/dashboard",
+    name: "Scripts"
   }
-
 ]
